8d69f07ce53750201d6b4deb89e6f07702d76f7a
Sai Kumar Basaveswara <saikumar@Sais-MacBook-Pro.local>
2021-10-31 22:07:38 +0530

    Added CORS Filter in server.js

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const { createServer } = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.static('public'))
 
 //PORT ENVIRONMENT VARIABLE
