a13309811fa7b405136db60e93ff31101d514aa1
Sai Kumar Basaveswara <saikumar@Sais-MBP.lan>
2021-11-01 07:29:26 +0530

    cors package added

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 // const crypto = require("crypto");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
+app.use(cors())
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
